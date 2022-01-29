bff9977efc35a6d3601dc661f7b67ccc5a9eeb62
Andrey <steamprofile2020@gmail.com>
2022-01-29 15:59:29 +0300

    components for the dropdown list

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -27,9 +27,11 @@
       </button>
       <button class="relative group p-2 focus:outline-none">
         <BaseIcon name="viewGrid" class="w-5 h-5" />
+        <TheDropdownApps />
       </button>
       <button class="relative group p-2 focus:outline-none">
         <BaseIcon name="dotsVertical" class="w-5 h-5" />
+        <!-- <TheDropdownSettings /> -->
       </button>
       <ButtonLogin />
     </div>
